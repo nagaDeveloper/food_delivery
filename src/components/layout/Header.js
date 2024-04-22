95919086bb63028c742c4a84c75a91c634c4b269
nagaDeveloper <naga.developer@gmail.com>
2024-04-22 21:42:09 +0530

    added profile page and conditional rendering

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,6 +1,14 @@
+"use client";
+import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 
 export default function Header() {
+  console.log(useSession());
+  const { status, data } = useSession();
+  const { user } = data ?? {};
+  const { email } = user ?? {};
+  const username = email ? email.split("@")[0] : "";
+
   return (
     <header className="flex items-center justify-between">
       <nav className="flex items-center gap-8 text-gray-500 font-semibold">
@@ -13,13 +21,30 @@ export default function Header() {
         <Link href={""}>Contact</Link>
       </nav>
       <nav className="flex items-center gap-4 text-gray-500 font-semibold">
-        <Link href={"/login"}>Login</Link>
-        <Link
-          className="bg-primary text-white rounded-full px-8 py-2"
-          href={"/register"}
-        >
-          Register
-        </Link>
+        {status === "authenticated" && (
+          <>
+            <Link href={"/profile"} className="whitespace-nowrap">
+              Hello {username}
+            </Link>
+            <button
+              onClick={() => signOut()}
+              className="bg-primary rounded-full text-white px-8 py-2"
+            >
+              Logout
+            </button>
+          </>
+        )}
+        {status === "unauthenticated" && (
+          <>
+            <Link href={"/login"}>Login</Link>
+            <Link
+              className="bg-primary text-white rounded-full px-8 py-2"
+              href={"/register"}
+            >
+              Register
+            </Link>
+          </>
+        )}
       </nav>
     </header>
   );
